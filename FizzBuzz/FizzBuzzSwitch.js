536647d7e23fc1c5be14992f2f939f7dec03a90f
Johann Taylor <j.taylor1343@gmail.com>
2021-10-09 23:57:39 -0400

    Added pseudocode

--- FizzBuzz/FizzBuzzSwitch.js
+++ FizzBuzz/FizzBuzzSwitch.js
@@ -1,3 +1,12 @@
+// Define n
+// Create function
+// Create an output array to store values
+// Create values for Fizz and Buzz and set them to false
+// Create a loop to count values from 1 to n
+// Check if values for Fizz and Buzz have remainders.. if i%3 and/or i%5 has no remainder then it is true, else false
+// Use switch statement to evalute if conditions are true to print either, FizzBuzz, Fizz, or Buzz
+// Set default to print any non divisible numbers as strings
+
 let n = 15;
 
 let fizzBuzz = () => {
---
+++ FizzBuzz/FizzBuzzTernary.js
@@ -0,0 +1,7 @@
+let n = 15;
+
+let fizzBuzz = () => {
+    for (let i=1; i<=n; i++) {
+        
+    }
+}
