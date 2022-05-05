65888d6345d86f089744b4fd194bb6da6dcc8416
johannt91 <65516487+johannt91@users.noreply.github.com>
2022-05-05 13:03:44 -0400

    Cleaned up code

--- ArrayConcatenation/index.js
+++ ArrayConcatenation/index.js
@@ -1,29 +1,28 @@
 let nums = [ 1, 2, 3, 4, 5];
 
 // Using for loop
-getConcatenation = () => {
+const firstConcatMethod = () => {
     let result = [];
     for (let i = 0; i<=nums.length-1; i++) {
         result[i] = nums[i];
         result[i + nums.length] = nums[i];
     }
-    console.log(result);
     return result;
 };
 
 
 // Using concat method
-getConcatenation = () => {
+const secondConcatMethod = () => {
     let ans = nums.concat(nums)
-    console.log(ans);
     return ans;
 };
 
 
 // Using spread
-getConcatenation = () => {
-    console.log([...nums, ...nums]);
+const thirdConcatMethod = () => {
     return [...nums, ...nums]
 };
 
-getConcatenation(nums);
+console.log(firstConcatMethod(nums));
+console.log(SecondConcatMethod(nums));
+console.log(ThirdConcatMethod(nums));
